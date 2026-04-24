9edd167964c7138b859496757a61933cd5d3f3c4
Andy-J-B <080.abae@gmail.com>
2026-04-24 10:05:05 -0700

    Finish tcp_connection class

--- src/tcp_connection.cpp
+++ src/tcp_connection.cpp
@@ -3,20 +3,38 @@
 #include <memory.h>
 #include <netinet/tcp.h>
 
+std::string make_daytime_string() {
+  using namespace std;
+  time_t now = time(0);
+  return ctime(&now);
+}
+
 namespace http {
 namespace server {
 
 typedef std::shared_ptr<tcp_connection> pointer;
 
-static pointer create(boost::asio::io_context &io_context);
+pointer tcp_connection::create(boost::asio::io_context &io_context) {
+  return pointer(new tcp_connection(io_context));
+}
 
-boost::asio::ip::tcp::socket &socket();
+boost::asio::ip::tcp::socket &tcp_connection::socket() { return socket_; }
 
-void start();
+void tcp_connection::start() {
+  message_ = make_daytime_string();
+  boost::asio::async_write(
+      socket_, boost::asio::buffer(message_),
+      std::bind(&tcp_connection::handle_write, shared_from_this(),
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+}
 
-tcp_connection(boost::asio::io_context &io_context);
+tcp_connection::tcp_connection(boost::asio::io_context &io_context)
+    : socket_(io_context) {
+  // Empty
+}
 
-void handle_write(const boost::system::error_code &);
+void handle_write(const boost::system::error_code &, size_t) {}
 
 } // namespace server
 } // namespace http
