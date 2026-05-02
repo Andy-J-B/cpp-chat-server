929ed1850fe7407c6b897ad1e1353dda0f45a58d
Andy-J-B <080.abae@gmail.com>
2026-05-02 16:04:36 -0700

    Update client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -1,1 +1,42 @@
-int main() { return 0; }
+#include <array>
+#include <boost/asio.hpp>
+#include <exception>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    perror("Was expecting arguments <server host address>");
+    exit(EXIT_FAILURE);
+  }
+
+  try {
+    boost::asio::io_context io_context;
+
+    // convert server name to tcp endpoint
+    boost::asio::ip::tcp::resolver resolver(io_context);
+    boost::asio::ip::tcp::resolver::results_type endpoints =
+        resolver.resolve(argv[1], "daytime");
+
+    // try both ipv4 and ipv6
+    boost::asio::ip::tcp::socket socket(io_context);
+    boost::asio::connect(socket, endpoints);
+
+    // read input
+    for (;;) {
+      std::array<char, 128> buf;
+      boost::system::error_code error;
+
+      size_t len = socket.read_some(boost::asio::buffer(buf), error);
+      if (error == boost::asio::error::eof) {
+        break;
+      } else if (error) {
+        throw boost::system::system_error(error);
+      }
+      std::cout.write(buf.data(), len);
+    }
+  } catch (std::exception &err) {
+    std::cerr << err.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return 0;
+}
